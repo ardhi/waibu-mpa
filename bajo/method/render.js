2f12dc95e35b2ff5f8b32367cb0c481d003c0f09
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-31 09:05:48 +0700

    Reorganize & custom hacks for c:include

--- bajo/method/render.js
+++ bajo/method/render.js
@@ -1,12 +1,12 @@
 import path from 'path'
 import applyFormat from '../../lib/apply-format.js'
 
-async function render (tpl, params = {}, reply, opts = {}) {
+async function render (tpl, locals = {}, reply, opts = {}) {
   const ext = path.extname(tpl)
   const viewEngine = this.getViewEngine(ext)
-  const text = await viewEngine.render(tpl, params, reply, opts)
+  const text = await viewEngine.render(tpl, locals, reply, opts)
   if (ext === '.md') opts.markdown = true
-  return applyFormat.call(this, { text, ext, opts, reply })
+  return applyFormat.call(this, { text, ext, opts, reply, locals })
 }
 
 export default render
