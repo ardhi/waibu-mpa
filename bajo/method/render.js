7dfde11214b622e195cb8d1cc9883d325bada046
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-06 21:54:04 +0700

    Add hooks

--- bajo/method/render.js
+++ bajo/method/render.js
@@ -2,11 +2,16 @@ import path from 'path'
 import applyFormat from '../../lib/apply-format.js'
 
 async function render (tpl, locals = {}, reply, opts = {}) {
+  const { runHook } = this.app.bajo
+
+  await runHook(`${this.name}:beforeRender`, { tpl, locals, reply, opts })
   const ext = path.extname(tpl)
   const viewEngine = this.getViewEngine(ext)
   const text = await viewEngine.render(tpl, locals, reply, opts)
   if (ext === '.md') opts.markdown = true
-  return await applyFormat.call(this, { text, ext, opts, reply, locals })
+  const result = await applyFormat.call(this, { text, ext, opts, reply, locals })
+  await runHook(`${this.name}:afterRender`, { tpl, locals, reply, opts, ext, result })
+  return result
 }
 
 export default render
