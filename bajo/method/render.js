4753910441194a8703862ebfcdefc4391c6a1365
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-09 22:22:12 +0700

    Cache feature

--- bajo/method/render.js
+++ bajo/method/render.js
@@ -1,9 +1,15 @@
 import path from 'path'
+import crypto from 'crypto'
 import applyFormat from '../../lib/apply-format.js'
 
 async function render (tpl, locals = {}, reply, opts = {}) {
   const { runHook } = this.app.bajo
-
+  const cache = this.app.bajoCache
+  const key = crypto.createHash('md5').update(`${tpl}:${JSON.stringify(locals)}`).digest('hex')
+  if (cache) {
+    const item = await cache.get({ key })
+    if (item) return item
+  }
   await runHook(`${this.name}:beforeRender`, { tpl, locals, reply, opts })
   const ext = path.extname(tpl)
   const viewEngine = this.getViewEngine(ext)
@@ -11,6 +17,7 @@ async function render (tpl, locals = {}, reply, opts = {}) {
   if (ext === '.md') opts.markdown = true
   const result = await applyFormat.call(this, { text, ext, opts, reply, locals })
   await runHook(`${this.name}:afterRender`, { tpl, locals, reply, opts, ext, result })
+  if (cache) await cache.set({ key, value: result, ttl: this.config.page.cacheMaxAge * 1000 })
   return result
 }
 
