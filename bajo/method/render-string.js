68c5e75cb496b9e051ef5f4f6d3c09f750e71324
Ardhi Lukianto <ardhi@lukianto.com>
2024-08-13 20:45:43 +0700

    Refactor

--- bajo/method/render-string.js
+++ bajo/method/render-string.js
@@ -1,7 +1,10 @@
 import applyFormat from '../../lib/apply-format.js'
+import buildLocals from '../../lib/build-locals.js'
 
-async function renderString (text, locals = {}, reply, opts = {}) {
+async function renderString (text, params = {}, reply, opts = {}) {
+  const { merge } = this.app.bajo.lib._
   const ve = this.getViewEngine(opts.ext)
+  const locals = await buildLocals.call(this, null, merge({}, params, { opts }), reply)
   text = await ve.renderString(text, locals, reply, opts)
   return await applyFormat.call(this, { text, ext: opts.ext, reply, opts, locals })
 }
--- bajo/method/render.js
+++ bajo/method/render.js
@@ -1,10 +1,13 @@
 import path from 'path'
 import crypto from 'crypto'
 import applyFormat from '../../lib/apply-format.js'
+import buildLocals from '../../lib/build-locals.js'
 
-async function render (tpl, locals = {}, reply, opts = {}) {
+async function render (tpl, params = {}, reply, opts = {}) {
   const { runHook } = this.app.bajo
+  const { merge } = this.app.bajo.lib._
   const cache = this.app.bajoCache
+  const locals = await buildLocals.call(this, tpl, merge({}, params, opts), reply)
   const key = crypto.createHash('md5').update(`${tpl}:${JSON.stringify(locals)}`).digest('hex')
   if (cache) {
     const item = await cache.get({ key })
@@ -14,7 +17,6 @@ async function render (tpl, locals = {}, reply, opts = {}) {
   const ext = path.extname(tpl)
   const viewEngine = this.getViewEngine(ext)
   const text = await viewEngine.render(tpl, locals, reply, opts)
-  if (ext === '.md') opts.markdown = true
   const result = await applyFormat.call(this, { text, ext, opts, reply, locals })
   await runHook(`${this.name}:afterRender`, { tpl, locals, reply, opts, ext, result })
   if (cache) await cache.set({ key, value: result, ttl: this.config.page.cacheMaxAge * 1000 })
