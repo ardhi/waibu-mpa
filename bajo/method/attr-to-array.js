9cefe60ebb93089a905bdde78f4ad7c907ee6840
Ardhi Lukianto <ardhi@lukianto.com>
2024-09-02 21:18:27 +0700

    Update attr-to-array.js

--- bajo/method/attr-to-array.js
+++ bajo/method/attr-to-array.js
@@ -1,6 +1,7 @@
 function attrToArray (text = '', delimiter = ' ') {
-  const { map, trim, without } = this.app.bajo.lib._
-  return without(map(text.split(delimiter), i => trim(i)), '')
+  const { map, trim, without, isArray } = this.app.bajo.lib._
+  if (isArray(text)) text = text.join(delimiter)
+  return without(map(text.split(delimiter), i => trim(i)), '', undefined, null)
 }
 
 export default attrToArray
