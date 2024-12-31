936d720aa1c376ff8deec5ca9fd757ae68302993
Ardhi Lukianto <ardhi@lukianto.com>
2024-12-31 20:52:50 +0700

    Updates

--- bajo/method/build-url.js
+++ bajo/method/build-url.js
@@ -1,21 +1,27 @@
 function buildUrl ({ exclude = [], prefix = '?', base, url = '', params = {} }) {
   const { qs } = this.app.waibu
-  const { forOwn, omit } = this.app.bajo.lib._
+  const { forOwn, omit, isEmpty } = this.app.bajo.lib._
   const qsKey = this.app.waibu.config.qsKey
-  let [path, query] = url.split('?')
+  let path
+  let hash
+  let query
+  [path, hash] = url.split('#')
+  if (hash.includes('?')) [hash, query] = hash.split('?')
+  else [path, query] = path.split('?')
   query = qs.parse(query) ?? {}
   forOwn(params, (v, k) => {
     const key = qsKey[k] ?? k
     query[key] = v
   })
   query = prefix + qs.stringify(omit(query, exclude))
-  if (!base) return path + query
+  if (!isEmpty(hash)) hash = '#' + hash
+  if (!base) return path + query + hash
   const parts = path.split('/')
   if (base) {
     parts.pop()
     parts.push(base)
   }
-  return parts.join('/') + query
+  return parts.join('/') + query + hash
 }
 
 export default buildUrl
