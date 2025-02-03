d5eeb678a3fb423bfc74deed35e6bf26e6ed88ec
Ardhi Lukianto <ardhi@lukianto.com>
2025-02-03 22:10:45 +0700

    Updates

--- bajo/method/resolve-layout.js
+++ bajo/method/resolve-layout.js
@@ -1,10 +1,23 @@
 import resolveResource, { filecheck } from '../../lib/resolve-resource.js'
 
-function fallbackHandler ({ file, dir, base, exts, ns, subSubNs, type }) {
+function fallbackHandler ({ file, exts, ns, subSubNs, type, theme }) {
+  const dir = ''
+  const base = 'default'
+  if (!this.config.viewEngine.layout.fallback) return false
+  // check main: theme specific
+  if (theme && !file) {
+    const check = `${this.app.main.dir.pkg}/${this.name}/${type}/_${theme.name}`
+    file = filecheck.call(this, { dir, base, exts, check })
+  }
+  // check mail: common
+  if (!file) {
+    const check = `${this.app.main.dir.pkg}/${this.name}/${type}`
+    file = filecheck.call(this, { dir, base, exts, check })
+  }
   // check fallback: common
-  if (!file && this.config.viewEngine.layout.fallback) file = filecheck.call(this, { dir: '', base: 'default', exts, check: `${this.app[ns].dir.pkg}/${this.name}/${subSubNs ? (subSubNs + '/') : ''}${type}` })
+  if (!file) file = filecheck.call(this, { dir, base, exts, check: `${this.app[ns].dir.pkg}/${this.name}/${subSubNs ? (subSubNs + '/') : ''}${type}` })
   // check general fallback
-  if (!file && this.config.viewEngine.layout.fallback) file = filecheck.call(this, { dir: '', base: 'default', exts, check: `${this.dir.pkg}/${this.name}/${subSubNs ? (subSubNs + '/') : ''}${type}` })
+  if (!file) file = filecheck.call(this, { dir, base, exts, check: `${this.dir.pkg}/${this.name}/${subSubNs ? (subSubNs + '/') : ''}${type}` })
   return file
 }
 
