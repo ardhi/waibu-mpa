1af7a6445e32415eef3802323fa16d63430a0b4a
Ardhi Lukianto <ardhi@lukianto.com>
2025-02-28 22:28:27 +0700

    Updates

--- bajo/method/render-string.js
+++ bajo/method/render-string.js
@@ -1,11 +1,9 @@
-import applyFormat from '../../lib/apply-format.js'
 import buildLocals from '../../lib/build-locals.js'
 
 async function renderString (text, params = {}, opts = {}) {
-  const locals = await buildLocals.call(this, { template: null, params, opts })
+  const locals = await buildLocals.call(this, { tpl: null, params, opts })
   const ve = this.getViewEngine(opts.ext)
-  text = await ve.renderString(text, locals, opts)
-  return await applyFormat.call(this, { text, ext: opts.ext, opts, locals })
+  return await ve.renderString(text, locals, opts)
 }
 
 export default renderString
--- bajo/method/render.js
+++ bajo/method/render.js
@@ -1,26 +1,13 @@
 import path from 'path'
-import crypto from 'crypto'
-import applyFormat from '../../lib/apply-format.js'
 import buildLocals from '../../lib/build-locals.js'
 
-async function render (template, params = {}, opts = {}) {
-  const { runHook } = this.app.bajo
-  const cache = this.app.bajoCache
-  const locals = await buildLocals.call(this, { template, params, opts })
-  const key = crypto.createHash('md5').update(`${template}:${JSON.stringify(locals)}`).digest('hex')
-  if (cache && !opts.partial) {
-    const item = await cache.get({ key })
-    if (item) return item
-  }
-  await runHook(`${this.name}:beforeRender${opts.partial ? 'Partial' : ''}`, { template, locals, opts })
-  const ext = path.extname(template)
+async function render (tpl, params = {}, opts = {}) {
+  const locals = await buildLocals.call(this, { tpl, params, opts })
+  const ext = path.extname(tpl)
   if (['.json', '.js', '.css'].includes(ext)) opts.partial = true
+  opts.ext = ext
   const viewEngine = this.getViewEngine(ext)
-  const text = await viewEngine.render(template, locals, opts)
-  const result = await applyFormat.call(this, { text, ext, opts, locals })
-  await runHook(`${this.name}:afterRender${opts.partial ? 'Partial' : ''}`, { template, locals, opts, result })
-  if (cache && !opts.partial) await cache.set({ key, value: result, ttl: this.config.page.cacheMaxAge * 1000 })
-  return result
+  return await viewEngine.render(tpl, locals, opts)
 }
 
 export default render
