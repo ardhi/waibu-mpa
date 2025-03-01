1109dfd10ed2a5d65d22313708e18583932bf192
Ardhi Lukianto <ardhi@lukianto.com>
2025-03-01 22:40:32 +0700

    Updates

--- bajo/method/render-string.js
+++ bajo/method/render-string.js
@@ -1,6 +1,6 @@
-import buildLocals from '../../lib/build-locals.js'
-
 async function renderString (text, params = {}, opts = {}) {
+  const { importModule } = this.app.bajo
+  const buildLocals = await importModule('waibu:/lib/build-locals.js')
   const locals = await buildLocals.call(this, { tpl: null, params, opts })
   const ve = this.getViewEngine(opts.ext)
   return await ve.renderString(text, locals, opts)
--- bajo/method/render.js
+++ bajo/method/render.js
@@ -1,11 +1,13 @@
 import path from 'path'
-import buildLocals from '../../lib/build-locals.js'
 
 async function render (tpl, params = {}, opts = {}) {
+  const { importModule } = this.app.bajo
+  const buildLocals = await importModule('waibu:/lib/build-locals.js')
   const locals = await buildLocals.call(this, { tpl, params, opts })
   const ext = path.extname(tpl)
   if (['.json', '.js', '.css'].includes(ext)) opts.partial = true
   opts.ext = ext
+  opts.cacheMaxAge = this.config.page.cacheMaxAgeDur
   const viewEngine = this.getViewEngine(ext)
   return await viewEngine.render(tpl, locals, opts)
 }
