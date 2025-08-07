3c59c09f2d44526accb5071eef31878b2afa01eb
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-07 17:36:36 +0700

    Updates

--- waibuMpa/route/logo/@id.js
+++ waibuMpa/route/logo/@id.js
@@ -1,23 +1,20 @@
-import path from 'path'
-
-async function logo (req, reply) {
-  const { importPkg } = this.app.bajo
-  const mime = await importPkg('waibu:mime')
+async function resolveFile (req) {
   const { getPlugin } = this.app.bajo
   const { camelCase } = this.lib._
-  const { fastGlob, fs } = this.lib
+  const { fastGlob } = this.lib
   const id = camelCase(req.params.id)
   const plugin = getPlugin(id)
   let type = ''
   if (req.query.type) type = `-${req.query.type}`
   const files = await fastGlob(`${plugin.dir.pkg}/logo${type}.*`)
   if (files.length === 0) throw this.error('_notFound')
-  const file = files[0]
-  const mimeType = mime.getType(path.extname(file))
-  reply.header('Content-Type', mimeType)
-  const stream = fs.createReadStream(file)
-  reply.send(stream)
-  return reply
+  return files[0]
+}
+
+async function logo (req, reply) {
+  const { importModule } = this.app.bajo
+  const handler = await importModule('waibu:/lib/handle-download.js')
+  return await handler.call(this, resolveFile, req, reply)
 }
 
 export default logo
