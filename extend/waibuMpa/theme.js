9d636ca85a1d26ad937b1bed0402d369d302ab16
ardhi <ardhi.lukianto@gmail.com>
2026-01-02 10:06:56 +0700

    Updates

---
+++ extend/waibuMpa/theme.js
@@ -0,0 +1,16 @@
+async function theme (ctx) {
+  const css = [
+    'waibuMpa.virtual:/purecss/pure-min.css'
+  ]
+  const meta = [{
+    name: 'viewport',
+    content: 'width=device-width, initial-scale=1'
+  }]
+  return {
+    name: 'default',
+    css,
+    meta
+  }
+}
+
+export default theme
