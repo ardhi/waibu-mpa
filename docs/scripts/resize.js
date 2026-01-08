bd2a80ab9e7ba49ba599bd6721c68613a521533a
ardhi <ardhi.lukianto@gmail.com>
2026-01-08 08:57:24 +0700

    Updates

--- docs/scripts/resize.js
+++ docs/scripts/resize.js
@@ -1,36 +1,36 @@
 /* global document */
 // This file is @deprecated
 
-let NAVBAR_OPTIONS = {};
+var NAVBAR_OPTIONS = {};
 
-(function () {
-  const NAVBAR_RESIZE_LOCAL_STORAGE_KEY = 'NAVBAR_RESIZE_LOCAL_STORAGE_KEY'
+(function() {
+  var NAVBAR_RESIZE_LOCAL_STORAGE_KEY = 'NAVBAR_RESIZE_LOCAL_STORAGE_KEY';
 
-  const navbar = document.querySelector('#navbar')
-  const footer = document.querySelector('#footer')
-  const mainSection = document.querySelector('#main')
-  const localStorageResizeObject = JSON.parse(
+  var navbar = document.querySelector('#navbar');
+  var footer = document.querySelector('#footer');
+  var mainSection = document.querySelector('#main');
+  var localStorageResizeObject = JSON.parse(
     // eslint-disable-next-line no-undef
     localStorage.getItem(NAVBAR_RESIZE_LOCAL_STORAGE_KEY)
-  )
+  );
 
   /**
      * Check whether we have any resize value in local storage or not.
      * If we have resize value then resize the navbar.
      **/
   if (localStorageResizeObject) {
-    navbar.style.width = localStorageResizeObject.width
-    mainSection.style.marginLeft = localStorageResizeObject.width
-    footer.style.marginLeft = localStorageResizeObject.width
+    navbar.style.width = localStorageResizeObject.width;
+    mainSection.style.marginLeft = localStorageResizeObject.width;
+    footer.style.marginLeft = localStorageResizeObject.width;
   }
 
-  const navbarSlider = document.querySelector('#navbar-resize')
+  var navbarSlider = document.querySelector('#navbar-resize');
 
-  function resizeNavbar (event) {
-    const pageX = event.pageX
-    const pageXPlusPx = event.pageX + 'px'
-    let min = Number.parseInt(NAVBAR_OPTIONS.min, 10) || 300
-    const max = Number.parseInt(NAVBAR_OPTIONS.max, 10) || 600
+  function resizeNavbar(event) {
+    var pageX = event.pageX,
+      pageXPlusPx = event.pageX + 'px',
+      min = Number.parseInt(NAVBAR_OPTIONS.min, 10) || 300,
+      max = Number.parseInt(NAVBAR_OPTIONS.max, 10) || 600;
 
     /**
          * Just to add some checks. If min is smaller than 10 then
@@ -39,7 +39,7 @@ let NAVBAR_OPTIONS = {};
          * because navbar slider will be hidden.
          */
     if (min < 10) {
-      min = 10
+      min = 10;
     }
 
     /**
@@ -47,44 +47,44 @@ let NAVBAR_OPTIONS = {};
          * allowed value.
          */
     if (min < pageX && pageX < max) {
-      navbar.style.width = pageXPlusPx
-      mainSection.style.marginLeft = pageXPlusPx
-      footer.style.marginLeft = pageXPlusPx
+      navbar.style.width = pageXPlusPx;
+      mainSection.style.marginLeft = pageXPlusPx;
+      footer.style.marginLeft = pageXPlusPx;
     }
   }
 
-  function setupEventListeners () {
+  function setupEventListeners() {
     // eslint-disable-next-line no-undef
-    window.addEventListener('mousemove', resizeNavbar)
+    window.addEventListener('mousemove', resizeNavbar);
     // eslint-disable-next-line no-undef
-    window.addEventListener('touchmove', resizeNavbar)
+    window.addEventListener('touchmove', resizeNavbar);
   }
 
-  function afterRemovingEventListeners () {
+  function afterRemovingEventListeners() {
     // eslint-disable-next-line no-undef
     localStorage.setItem(
       NAVBAR_RESIZE_LOCAL_STORAGE_KEY,
       JSON.stringify({
         width: navbar.style.width
       })
-    )
+    );
   }
 
-  function removeEventListeners () {
+  function removeEventListeners() {
     // eslint-disable-next-line no-undef
-    window.removeEventListener('mousemove', resizeNavbar)
+    window.removeEventListener('mousemove', resizeNavbar);
     // eslint-disable-next-line no-undef
-    window.removeEventListener('touchend', resizeNavbar)
-    afterRemovingEventListeners()
+    window.removeEventListener('touchend', resizeNavbar);
+    afterRemovingEventListeners();
   }
 
-  navbarSlider.addEventListener('mousedown', setupEventListeners)
-  navbarSlider.addEventListener('touchstart', setupEventListeners)
+  navbarSlider.addEventListener('mousedown', setupEventListeners);
+  navbarSlider.addEventListener('touchstart', setupEventListeners);
   // eslint-disable-next-line no-undef
-  window.addEventListener('mouseup', removeEventListeners)
-})()
+  window.addEventListener('mouseup', removeEventListeners);
+})();
 
 // eslint-disable-next-line no-unused-vars
-function setupResizeOptions (options) {
-  NAVBAR_OPTIONS = options
+function setupResizeOptions(options) {
+  NAVBAR_OPTIONS = options;
 }
--- wiki/CHANGES.md
+++ wiki/CHANGES.md
@@ -1,5 +1,11 @@
 # Changes
 
+## 2025-01-08
+
+- [2.1.0] Upgrade to ```node-emoji@2.2.0```
+- [2.1.0] Upgrade to ```prettier@2.7.4```
+- [2.1.0] Upgrade to ```tring-strip-html@13.5.0```
+
 ## 2025-12-30
 
 - [2.1.0] Ported to match ```bajo@2.2.x``` specs
